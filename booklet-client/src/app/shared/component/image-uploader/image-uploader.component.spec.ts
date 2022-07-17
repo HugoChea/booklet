476319c568d23136d9c01b3f92cecf5dd2ca57e8
HugoChea <hugochea.pro@outlook.com>
2022-07-17 18:58:53 +0200

    fix test imports

--- booklet-client/src/app/shared/component/image-uploader/image-uploader.component.spec.ts
+++ booklet-client/src/app/shared/component/image-uploader/image-uploader.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@shared/material.module';
 
 import { ImageUploaderComponent } from './image-uploader.component';
 
@@ -8,7 +10,8 @@ describe('ImageUploaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ImageUploaderComponent ]
+      declarations: [ ImageUploaderComponent ],
+      imports: [MaterialModule, BrowserAnimationsModule],
     })
     .compileComponents();
 
