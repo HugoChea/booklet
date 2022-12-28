e8ae9e7a50a976ebf0271db118a15adb20855cfb
HugoChea <hugochea.pro@outlook.com>
2022-12-29 00:17:36 +0100

    upgrade nest js and added swagger

--- booklet-api/src/main.ts
+++ booklet-api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { initializeApp } from "firebase/app";
 import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,15 @@ async function bootstrap() {
 
   console.log(firebaseConfig)
   initializeApp(firebaseConfig);
+
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('Booklet API')
+  .setDescription('Book features management')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(3000);
 }
