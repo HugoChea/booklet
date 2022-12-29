328a50c55cd3f92f1520552230514dc9a2a8b0a6
HugoChea <hugochea.pro@outlook.com>
2022-12-29 17:45:49 +0100

    added generic http type and fixed all api calls

--- booklet-client/src/app/core/services/book.service.ts
+++ booklet-client/src/app/core/services/book.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpApiResponse } from '@core/models/http/http-api-response';
 import { environment } from '@env';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { CreateBookDto } from '../dto/create-book-dto';
 import { Book } from '../models/book/book';
 
@@ -15,10 +16,12 @@ export class BookService {
   constructor(private http: HttpClient) { }
 
   createBook(createBook: CreateBookDto): Observable<CreateBookDto> {
-    return this.http.post<CreateBookDto>(this.apiURL, createBook);
+    return this.http.post<HttpApiResponse<CreateBookDto>>(this.apiURL, createBook)
+      .pipe(map((res) => res.data));
   }
 
   getListBookByUser(userId: string): Observable<Book[]> {
-    return this.http.get<Book[]>(this.apiURL + "/list/" + userId);
+    return this.http.get<HttpApiResponse<Book[]>>(this.apiURL + "/list/" + userId)
+      .pipe(map((res) => res.data));
   }
 }
