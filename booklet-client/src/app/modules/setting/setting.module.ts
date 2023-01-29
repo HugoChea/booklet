c55eef7024870627e3ac46ff4ae183dc00f802c3
HugoChea <hugochea.pro@outlook.com>
2023-01-29 16:08:39 +0100

    bkl-73: updated navbar

---
+++ booklet-client/src/app/modules/setting/setting.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { SettingRoutingModule } from './setting-routing.module';
+import { SharedModule } from '@shared/shared.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule,
+    SettingRoutingModule
+  ]
+})
+export class SettingModule { }
