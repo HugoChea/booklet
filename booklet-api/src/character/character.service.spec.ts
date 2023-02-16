8a3a12954b81bf0bc9406d8d1e5962dce9a4939e
HugoChea <hugochea.pro@outlook.com>
2023-02-16 15:26:42 +0100

    update testing module

--- booklet-api/src/character/character.service.spec.ts
+++ booklet-api/src/character/character.service.spec.ts
@@ -1,6 +1,7 @@
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ImageUploaderService } from 'src/common/services/image-uploader/image-uploader.service';
+import { ParseUtilitiesService } from 'src/common/services/parse-utilities/parse-utilities.service';
 import { CharacterService } from './character.service';
 import { Character } from './schemas/character.schema';
 
@@ -18,6 +19,7 @@ describe('CharacterService', () => {
       providers: [
         CharacterService,
         ImageUploaderService,
+        ParseUtilitiesService,
         {provide: getModelToken(Character.name), useValue: mockRepository},
       ],
     }).compile();
